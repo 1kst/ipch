93799723279ae1261b16165d7e411495684f3c1d
1kst <fxcktg@gmail.com>
2024-07-28 09:10:54 +0800

    Create 1001.sh

---
+++ 1001.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+ADDR="qq.com"
+TMPSTR=$(ping -c 1 ${ADDR} | sed '1{s/[^(]*(//;s/).*//;q}')
+ports=(22 80 443) # 添加你想要监测的端口
+check_ip=0
+
+for port in ${ports[@]}
+do
+    for i in {1..3}
+    do
+        check=$(nmap ${TMPSTR} -p $port | grep open | wc -l)
+        if [ $check -eq 1 ]; then 
+            check_ip=1
+            break
+        fi
+        sleep 10
+    done
+    if [ $check_ip -eq 1 ]; then 
+        break
+    fi
+done
+
+if [ $check_ip -eq 0 ]; then 
+    echo "Ports are not open. Triggering curl."
+    curl "https://api.pqs.pw/ipch/********" >/dev/null 2>&1 &
+    nscd -i hosts
+    nscd -i passwd
+    nscd -i group
+    /etc/init.d/nscd restart
+fi
+
+echo "IP Address: ${TMPSTR}"
